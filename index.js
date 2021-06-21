08dd4e0375c89b785eb7f9e5f5240cc65c79b0aa
TechnologicNick <technologicnick@gmail.com>
2021-06-22 00:22:15 +0200

    List all decompressed buffers
    slice_start=12, slice_end=175

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 const SQL = require("sql-template-strings");
 const fs = require("fs");
-const { compress, decompress } = require("mini-lz4");
 const Blob = require("buffer").Blob;
+const LZ4 = require("lz4");
 
 
 (async () => {
@@ -26,25 +26,46 @@ const Blob = require("buffer").Blob;
     // console.log(`data`, data);
     // fs.writeFileSync("./data.bin", data);
 
-    for (let j = blobCompressed.size; j >= 0; j--) {
-        for (let i = 0; i <= blobCompressed.size; i++) {
+    let uncompressed = Buffer.alloc(128);
+
+    let decompressedMessages = {}
+
+    for (let i = 0; i <= blobCompressed.size; i++) {
+        for (let j = blobCompressed.size; j >= 0; j--) {
             try{
                 if (i >= j || i === 21) continue;
 
                 // let data = decompress(await blobCompressed.slice(i, j).arrayBuffer());
-                let buff = Buffer.concat([new Uint8Array([0x04, 0x22, 0x4D, 0x18]), dataCompressed.slice(i, j)]);
-                console.log(i, j, buff);
+                // let buff = Buffer.concat([new Uint8Array([0x04, 0x22, 0x4D, 0x18]), dataCompressed.slice(i, j)]);
+                let buff = dataCompressed.slice(i, j);
+                // console.log(i, j, buff);
 
-                let data = decompress(buff);
+                let uncompressedSize = LZ4.decodeBlock(buff, uncompressed)
+                // console.log(uncompressedSize);
+                let sliced = uncompressed.slice(0, uncompressedSize)
 
-                console.log(`i=${i} data`, data);
+
+                let str = sliced.toString();
+                // if (str.length > 0) console.log(`i=${i} j=${j} data`, str);
                 // fs.writeFileSync("./data.bin", blob);
+
+                decompressedMessages[`i=${i} j=${j}`] = str;
             } catch (ex) {
                 console.error(`i=${i} failed`, ex.message);
                 // throw ex;
             }
         }
+        // break;
     }
 
+    let filtered = Object.fromEntries(
+        Object.entries(decompressedMessages).filter(
+            ([key, value]) => {
+                return value.includes("$GAME_DATA/Scripts/game/worlds/CreativeFlatWorld.lua")
+            }
+        )
+    );
+
+    console.log(filtered);
 
 })();
