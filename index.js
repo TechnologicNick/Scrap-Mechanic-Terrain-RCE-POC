55c08fe1267f817e0365bfcf12af4afffa27396f
TechnologicNick <technologicnick@gmail.com>
2021-06-21 23:31:16 +0200

    Tried mini-lz4, doesn't support raw

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 const SQL = require("sql-template-strings");
 const fs = require("fs");
+const { compress, decompress } = require("mini-lz4");
+const Blob = require("buffer").Blob;
+
 
 (async () => {
     const db = await open({
@@ -13,7 +16,35 @@ const fs = require("fs");
     const { data: dataCompressed } = await db.get(SQL`SELECT data FROM GenericData WHERE channel = 11`);
 
     console.log("dataCompressed", dataCompressed);
-    fs.writeFileSync("./data.bin", dataCompressed);
+    fs.writeFileSync("./data.comp.bin", dataCompressed);
+
+    const blobCompressed = new Blob(dataCompressed);
 
 
-})()
+    // let data = decompress(Buffer.concat([new Uint8Array([0x04, 0x22, 0x4D, 0x18]), dataCompressed]));
+            
+    // console.log(`data`, data);
+    // fs.writeFileSync("./data.bin", data);
+
+    for (let j = blobCompressed.size; j >= 0; j--) {
+        for (let i = 0; i <= blobCompressed.size; i++) {
+            try{
+                if (i >= j || i === 21) continue;
+
+                // let data = decompress(await blobCompressed.slice(i, j).arrayBuffer());
+                let buff = Buffer.concat([new Uint8Array([0x04, 0x22, 0x4D, 0x18]), dataCompressed.slice(i, j)]);
+                console.log(i, j, buff);
+
+                let data = decompress(buff);
+
+                console.log(`i=${i} data`, data);
+                // fs.writeFileSync("./data.bin", blob);
+            } catch (ex) {
+                console.error(`i=${i} failed`, ex.message);
+                // throw ex;
+            }
+        }
+    }
+
+
+})();
