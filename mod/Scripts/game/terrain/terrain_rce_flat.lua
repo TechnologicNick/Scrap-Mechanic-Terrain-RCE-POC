3074440653ac0a9ef758c2efdd87edad96f3adf6
TechnologicNick <technologicnick@gmail.com>
2021-06-23 12:17:46 +0200

    Improve lua scripts

--- mod/Scripts/game/terrain/terrain_rce_flat.lua
+++ mod/Scripts/game/terrain/terrain_rce_flat.lua
@@ -1,3 +1,5 @@
-os.execute("start https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+-- Prove we're not sandboxed
+os.execute("start notepad.exe")
 
+-- Call the original file to prevent errors/crashes
 dofile("$GAME_DATA/Scripts/game/terrain/terrain_flat.lua")
